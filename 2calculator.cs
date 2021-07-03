0a069243becd4690384b5a9ad0d52c1e49d5b142
patrycja <patrycja@debian.com>
2021-07-03 12:36:41 +0200

    Completed calculator

--- 2calculator.cs
+++ 2calculator.cs
@@ -7,7 +7,14 @@ using System.Threading.Tasks;
 namespace Project {
     class Program {
         static void Main(string[] args) {
+            int num = Convert.ToInt32("45");
+            Console.WriteLine(num + 6);
+            Console.Write("Enter a number: ");
+            double num1 = Convert.ToDouble(Console.ReadLine());
+            Console.Write("Enter another number: ");
+            double num2 = Convert.ToDouble(Console.ReadLine());
 
+            Console.WriteLine(num1 + num2);
         }
     }
 }

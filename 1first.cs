aec7f21b6626a63ba8e0b5409bf97a60edd63155
patrycja <patrycja@debian.com>
2021-07-03 11:49:48 +0200

    String methods

--- 1first.cs
+++ 1first.cs
@@ -8,18 +8,31 @@ namespace Project {
     class Program {
         static void Main(string[] args) {
             string characterName = "John";
+            char grade = 'A';
             int characterAge;
             characterAge = 35;
+            int age = -30;
+            double gpa = 3.0;
+            bool isMale = true;
 
             Console.WriteLine("Hello World");
             Console.WriteLine("  /");
             Console.WriteLine(" /");
             Console.WriteLine("/");
+            Console.WriteLine(30);
+            Console.WriteLine(isMale);
+
+            Console.WriteLine("\n" + characterName.Length);
+            Console.WriteLine(characterName.ToUpper());
+            Console.WriteLine(characterName.ToLower());
+            Console.WriteLine(characterName.Contains("Jo"));
+            Console.WriteLine(characterName[2]);
+            Console.WriteLine(characterName.IndexOf("hn"));
+            Console.WriteLine(characterName.IndexOf("z"));
+            Console.WriteLine(characterName.Substring(1, 2));
 
             Console.WriteLine("\nThere once was a man named " + characterName);
             Console.WriteLine("He was " + characterAge + " years old");
-
-            Console.ReadLine();
         }
     }
 }

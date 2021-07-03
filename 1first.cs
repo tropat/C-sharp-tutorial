e046c45c9cdb4dbe91432e2ccfd6b0221a06d4a6
patrycja <patrycja@debian.com>
2021-07-03 11:39:12 +0200

    Added some story and variables

--- 1first.cs
+++ 1first.cs
@@ -7,10 +7,18 @@ using System.Threading.Tasks;
 namespace Project {
     class Program {
         static void Main(string[] args) {
+            string characterName = "John";
+            int characterAge;
+            characterAge = 35;
+
             Console.WriteLine("Hello World");
             Console.WriteLine("  /");
             Console.WriteLine(" /");
             Console.WriteLine("/");
+
+            Console.WriteLine("\nThere once was a man named " + characterName);
+            Console.WriteLine("He was " + characterAge + " years old");
+
             Console.ReadLine();
         }
     }
